7ed2841748f4de09564146cd35cd92bbca78129e
Krinsh Kava <krishsmith.nivzen@gmail.com>
2026-04-08 10:39:46 +0530

    first commit

--- storefront/settings.py
+++ storefront/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sessions',
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
@@ -87,8 +88,11 @@ WSGI_APPLICATION = 'storefront.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'storefront',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'rupal@2006'
     }
 }
 
